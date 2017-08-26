1e16acffacd39f960870bbf3f7c723376d4f0026
git9527 <zsn001@163.com>
2017-08-26 23:32:11 +0800

    完成updater.py

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 基于阿里云CDN API手册python版本签名机制代码示例[点击下载](https://docs-aliyun.cn-hangzhou.oss.aliyun-inc.com/cn/cdn/0.1.99/assets/api/callmethod_sdk_python.zip?spm=5176.doc27149.2.1.eijrGE&file=callmethod_sdk_python.zip)
 
 使用方法:
+
+1. 确保python已安装Requests(http://docs.python-requests.org/en/master/user/install/#install)
+2. 执行如下命令
+python updater.py -i LTAI**** -s aBPGz3**** -d www.***.*** -p ./certs/*.key -c ./certs/fullchain.cer
---
+++ updater.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+#coding:utf-8 
+
+import sys,os,inspect,subprocess,urlparse,time
+import requests
+import argparse
+
+def get_script_dir(follow_symlinks=True):
+	if getattr(sys, 'frozen', False): # py2exe, PyInstaller, cx_Freeze
+		path = os.path.abspath(sys.executable)
+	else:
+		path = inspect.getabsfile(get_script_dir)
+	if follow_symlinks:
+		path = os.path.realpath(path)
+	return os.path.dirname(path)
+
+def get_file_content_in_line(fileContainer):
+	result = fileContainer.readlines()
+	return ''.join(result)
+	
+def fire_update_request(args):
+	privateKey = get_file_content_in_line(args.privateKeyFile)
+	certificate = get_file_content_in_line(args.certificateFile)
+	domain = urlparse.urlparse("http://" + args.domainName)
+	subdomain = ".".join(domain.hostname.split('.')[:-2])
+	timestamp = time.strftime("%Y%m%d", time.gmtime())
+	certname = subdomain + "_" + timestamp
+	scriptPath = os.path.join(get_script_dir(), "cdn-api", "cdn.py")
+	url = subprocess.check_output(['python', scriptPath, "Action=SetDomainServerCertificate", "ServerCertificateStatus=on", "DomainName=" + args.domainName, "CertName=" + certname, "PrivateKey=" + privateKey, "ServerCertificate=" + certificate, "-i" + args.accessKeyId, "-s" + args.accessKeySecret]).rstrip()
+	resp = requests.get(url)
+	print resp.text
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Short sample app')
+	parser.add_argument("-i", dest="accessKeyId", help="Specify access key id")
+	parser.add_argument("-s", dest="accessKeySecret", help="Specify access key secret")
+	parser.add_argument("-d", dest="domainName", help="Specify CDN domain name")
+	parser.add_argument("-p", dest="privateKeyFile", help="Specify certificate private key file location", type=argparse.FileType('r'))
+	parser.add_argument("-c", dest="certificateFile", help="Specify certificate content file location", type=argparse.FileType('r'))
+
+	args = parser.parse_args()
+	
+	if not args.accessKeyId:
+		parser.error("Access key id required")
+	if not args.accessKeySecret:
+		parser.error("Access key secret required")
+	if not args.domainName:
+		parser.error("CDN domain name required")
+	if not args.privateKeyFile:
+		parser.error("Certificate private key required")
+	if not args.certificateFile:
+		parser.error("Certificate content required")
+	
+	fire_update_request(args)
+				
+	
+
+
+
+	
